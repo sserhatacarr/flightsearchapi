9de04a44dce46f60f84dba1ee1f94cbee2896a49
Serhat Acar <sserhatacarr@gmail.com>
2024-02-03 18:01:04 +0300

    Initial commit

---
+++ src/main/java/com/serhatacar/flightsearchapi/FlightsearchapiApplication.java
@@ -0,0 +1,13 @@
+package com.serhatacar.flightsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsearchapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsearchapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serhatacar/flightsearchapi/FlightsearchapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhatacar.flightsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsearchapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
